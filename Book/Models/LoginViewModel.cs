85058b691c8b094d3b02b6228dae73641cab51ea
bhaveshkumbhani010 <bhavesh@tabin.co.nz>
2024-01-06 17:11:39 +0530

    update design and add animation

--- Book/Models/LoginViewModel.cs
+++ Book/Models/LoginViewModel.cs
@@ -5,12 +5,12 @@ namespace Book.Models
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Username is required")]
+        //[Required(ErrorMessage = "Username is required")]
         public required string Username { get; set; }
 
        
-        [Required(ErrorMessage = "Password is required")]
-        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        //[Required(ErrorMessage = "Password is required")]
+        //[StringLength(50, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
         public required string Password { get; set; }
     }
 }
